6570d430bf0234596a89d9d331d51a8efbc8484d
tommyokoyo <okoyotommy@gmail.com>
2023-09-04 17:34:39 +0300

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,70 +1,52 @@
 #!/usr/bin/python3
-"""
-    script that reads stdin line by line and computes metrics
-"""
+
+'''
+Reads stdin line by line and computes metrics
+'''
 import sys
 
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-my_cache = []
+if __name__ == "__main__":
 
-
-def interrupt_handler():
-    print_metrics(my_cache[0], my_cache[1])
-    sys.exit(0)
-
-
-def print_metrics(total_file_size, status_code_counts):
-    if total_file_size and status_code_counts:
-        if my_cache:
-            my_cache[0] = total_file_size
-            my_cache[1] = status_code_counts
-            print(f"Total file size: {total_file_size}")
-            sorted_codes = sorted(status_code_counts.keys())
-            for code in sorted_codes:
-                print(f'{code}: {status_code_count[code]}')
-        else:
-            my_cache.append(total_file_size)
-            my_cache.append(status_code_counts)
-            print(f"Total file size: {total_file_size}")
-            sorted_codes = sorted(status_code_counts.keys())
-            for code in sorted_codes:
-                print(f'{code}: {status_code_count[code]}')
-    else:
-        pass
-
-
-def parse_input(line):
-    total_file_size = 0
-    line_count = 0
-    if line:
-        std_inputs = line.split()
-
-        if len(std_inputs) >= 9 and std_inputs[8].isdigit():
-            status_code = std_inputs[7]
-            file_size = std_inputs[8]
-
-            total_file_size += int(file_size)
-            line_count += 1
-
-            if status_code.isdigit():
-                status_code = int(status_code)
-                if status_code in status_codes:
-                    status_code_count[status_code] += 1
-                    return total_file_size, status_code_count
-                else:
-                    return
-
-
-if __name__ == '__main__':
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    file_size = [0]
     count = 1
-    my_list = []
+
+    def print_stats():
+        '''
+        Prints file size and stats for every 10 loops
+        '''
+        print('File size: {}'.format(file_size[0]))
+
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] != 0:
+                print('{}: {}'.format(code, status_codes[code]))
+
+    def parse_stdin(line):
+        '''
+        Checks the stdin for matches
+        '''
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
     try:
-        for values in sys.stdin:
-            value_1, value_2 = parse_input(values)
+        for line in sys.stdin:
+            parse_stdin(line)
+            # print the stats after every 10 outputs
             if count % 10 == 0:
-                print_metrics(value_1, value_2)
-            else:
-                count += 1
+                print_stats()
+            count += 1
     except KeyboardInterrupt:
+        print_stats()
         raise
+    print_stats()
