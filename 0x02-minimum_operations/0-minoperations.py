7b28953601229e471aeba5d13506cba36cb6c0c2
tommyokoyo <okoyotommy@gmail.com>
2023-09-04 17:44:45 +0300

    Min operators

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+
 """
-    Minimum Operators
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
 """
 
 
 def minOperations(n):
     """
-        Calculates the number of operations
-        :param n:
-        :return:
+    returns min operations to get n Hs
     """
-    if n <= 2:
+    result = 0
+    index = 2
+    if n < 2:
         return 0
-    else:
-        min_op = [0] * (n + 1)
-
-        for i in range(2, n + 1):
-            min_op[i] = float('inf')
-
-            for j in range(1, i):
-                if i % j == 0:
-                    min_op[i] = min(min_op[i], min_op[j] + (i // j))
-
-        return min_op[n]
+    while (index < n + 1):
+        # Check if problem is evenly brekadownable
+        while n % index == 0:
+            # If so add number of smaller problems to the result
+            result += index
+            # Create the smaller problem needed to get to n
+            n /= index
+        index += 1
+    return result
