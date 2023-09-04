4c19d7500e98917f5d562153fcd0bfd3993c13d0
tommyokoyo <okoyotommy@gmail.com>
2023-09-04 13:20:13 +0300

    Pascal's Triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n: int):
+    if n <= 0:
+        return []
+    else:
+        triangle = [[1]]
+
+        for i in range(1, n):
+            prev_row = triangle[-1]
+            new_row = [1]
+
+            for j in range(1, i):
+                new_element = prev_row[j - 1] + prev_row[j]
+                new_row.append(new_element)
+
+            new_row.append(1)
+            triangle.append(new_row)
+
+        return triangle
