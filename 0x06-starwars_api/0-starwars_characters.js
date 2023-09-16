46b64c533edfe7af0a6c85da72d5ec3f91e8bd7c
tommyokoyo <okoyotommy@gmail.com>
2023-09-16 18:07:40 +0300

    0x06. Star Wars API - Semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,13 +2,13 @@
 
 const request = require('request');
 
-function getMovieCharacters(movieId) {
+function getMovieCharacters (movieId) {
   const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
   request(apiUrl, { json: true }, (error, response, body) => {
     if (error) {
       console.error('Error: ', error);
-    } else if (response.statusCode !== 200){
-      console.error('Api call failed - status: ', response.statusCode)
+    } else if (response.statusCode !== 200) {
+      console.error('Api call failed - status: ', response.statusCode);
     } else {
       const movie_characters = body.characters;
 
@@ -17,7 +17,7 @@ function getMovieCharacters(movieId) {
   });
 }
 
-function fetchAndPrintCharacters(characters, index) {
+function fetchAndPrintCharacters (characters, index) {
   if (index >= characters.length) {
     return [];
   }
@@ -33,7 +33,7 @@ function fetchAndPrintCharacters(characters, index) {
       console.log(characterName);
       fetchAndPrintCharacters(characters, index + 1);
     }
-  })
+  });
 }
 
 const movieId = process.argv[2];
